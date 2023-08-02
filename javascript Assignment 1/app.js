8ce486dc7bf49c574cde93faf46f975b213c2845
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:42:19 +0500

    Javascript Assignment 1
    Javascript Assignment 1

---
+++ javascript Assignment 1/app.js
@@ -0,0 +1,84 @@
+// ASSIGNMENT NO 1
+// CHAPTER 1
+
+// alert("HELLO WORLD")
+// alert("WELCOME TO JS LAND" + "\n" + "HAPPY CODING" )
+
+// CHAPTER 2
+
+// var userName = "shoaib "
+// var myName = "Siddiqui"
+// var myAge = "16 years old"
+// var education = "1st year" 
+// var data = "mobile and cloud computing"
+// var fullname = userName + myName
+// alert(fullname)
+// alert(myAge)
+// alert(education)
+// alert(data)
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza)
+// var email = "my email address is" + " rasiddiqui970@gmail.com"
+// alert(email)
+// var book = "I am trying to learn from the book " + " 'A smarter way to learn javascripts'"
+// alert(book)
+// var text = "YAY! I CAN WRITE HTML CONTENT THROUGH JAVASCRIPT"
+// document.write(text)
+
+// CHAPTER 3 
+
+// var myAge = "I am 16 years old "
+// alert(myAge)
+// var visit = "You have visited this site 21 times"
+// alert(visit)
+// var birthyear = "My birth year is 1990"
+// var text = "Data type of my declared variable is number"
+// document.write(birthyear)
+// document.write("<br>")
+// document.write(text)
+// var item = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store."
+// document.write(item)
+
+// CHAPTER 4
+
+// var myName = "SHOAIB "
+// var fullName = "SIDDIQUI "
+// var myAge = 16
+// var calculate = myName + fullName + myAge
+// document.write(calculate)
+// 5 legal variables name
+// myvar = "shoaib"
+// my_var = "shoaib"
+// _my_var = "shoaib"
+// myVar = "shoaib"
+// MYVAR = "shoaib"
+// 5 illegal variables name
+// 2myvar = "Shoaib"
+// my-var = "Shoaib"
+// my var = "Shoaib"
+// alpha , numeric , underscores , dollar,
+// case sensitive
+// with a number
+
+// CHAPTER 5 TO 7
+
+// var num = 2
+// var number = 3
+// var totalnum = num + number
+// document.write(totalnum)
+// var num = 5
+// var number = 2
+// var totalnum = num - number
+// document.write(totalnum)
+// var num = 2
+// var number = 3
+// var totalnum = num * number
+// document.write(totalnum)
+// var num = 2
+// var number = 3
+// var totalnum = num % number
+// document.write(totalnum)
+// var num = 2
+// var number = 3
+// var totalnum = num / number
+// document.write(totalnum)
