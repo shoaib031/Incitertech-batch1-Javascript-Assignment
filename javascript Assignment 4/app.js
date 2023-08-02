0a74b4a08ba8f99152c3f5a190a66d5a9bab895c
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:43:30 +0500

    Javascript Assignment 4
    Javascript Assignment 4

---
+++ javascript Assignment 4/app.js
@@ -0,0 +1,93 @@
+var myArray = [];
+var myArray = new Array();
+myArray[0] = "Apple";
+myArray[1] = "Facebook";
+myArray[2] = "Google";
+myArray[3] = "Microsoft";
+document.write(myArray)
+var object = [];
+var object = new Array();
+object[0] = "montessori";
+object[1] = "school";
+object[2] = "college";
+object[3] = "uni";
+document.write(object)
+var empty = ["moheed" + "shoaib" + "wahaj" + "mughees"]
+document.write(empty + "<br>")
+var number = [1, 2, 3 , 4, 5]
+document.write(number + "<br>")
+var boolean = [true + false]
+document.write(boolean + "<br>")
+var mixed = ["shoaib", 16, + true]
+document.write(mixed + "<br>")
+var network = ["jazz " + "zong"]
+document.write(network + "<br>")
+var qualifications = "Qualifications :"
+document.write(qualifications + "<br>" + "<br>")
+var array = [1 + ") " + "SSC", 2 + ") " + "HSC", 3 + ") " + "BCS", 4 + ") " + "BS", 5 + ") " + "BCOM", 6 + ") " + "MS,M", 7 + ") " + "Phil", 8 + ") " + "PhD"]
+for (var i = 0; i <= array.length - 1; i++) {
+    document.write(array[i] + "<br>")
+}
+var movies = ("<b>Top movies of 2015</b>")
+document.write(movies + "<br>" + "<br>")
+
+var array = [1 + ") " + "Avengers : Age of ultron <br>"]
+var arra = [2 + ") " + "Spectre <br>"]
+var arr = [3 + ") " + "Jurrasic World <br>"]
+var ar = [4 + ") " + "Inside out <br>"]
+document.write(array)
+document.write(arra)
+document.write(arr)
+document.write(ar)
+var cars = ("<b>Favourites cars</b>") 
+document.write(cars + "<br>")
+var Favourite = ["Audi", "volvo", "ford", "lamborghini"]
+document.write(Favourite)
+var index = ("first index of the array: 0" + "<br>")
+var inde = ("var at first index of the array: Audi" + "<br>")
+var ind = ("Last index of the array: 3" + "<br>")
+var indx = ("car at last index of the array: Lamborghini" + "<br>")
+document.write(index)
+document.write(inde)
+document.write(ind)
+document.write(indx)
+var array = ["Micahael", "johny", "mughees"]
+var score = [320, 230, 480]
+var totalMarks = []
+
+for(i = 0; i < array.length; i++){
+    document.write(`score of ${array[i]} is ${score[i]}<br/>`);
+}
+var scores = [320, 230, 480, 120]
+document.write(scores + "<br>")
+scores.sort()
+document.write(scores)
+var list = ("Fruits list : <br>")
+document.write(list)
+var fruits = ["strawberry", "apple", "orange", "banana"]
+document.write(fruits + "<br>")
+fruits.sort()
+document.write(fruits)
+var array = ["this", "is", "my", "cat"]
+document.write(array + "<br>")
+array.join()
+document.write(array)
+var cat =  "Array :" + "<br>"
+var a = ["  This", "is",  "my", "cat"]
+var br =  "String :" + "<br>"
+var c = a.join( " ")
+document.write(cat)
+document.write(a + "<br>")
+document.write(br + "<br>")
+document.write(c)
+var devices = ["Devices :" + "<br>" + "keyboard", "mouse", "printer", "monitor"]
+document.write(devices + "<br>" + "<br>")
+var another = ["Out :"+ "<br>" + "Keyboard" + "<br>" + "Out :"+ "<br>" + "Mouse" + "<br>" + "Out :"+ "<br>" + "Printer"+ "<br>"  + "Out :"+ "<br>" + "Monitor" ]
+document.write(another)
+var devices = ["Devices :" + "<br>" + "keyboard", "mouse", "printer", "monitor"]
+document.write(devices + "<br>" + "<br>")
+var devices = ["Devices :" + "<br>" + "keyboard", "mouse", "printer", "monitor"]
+document.write(devices + "<br>" + "<br>")
+
+var another = ["Out :"+ "<br>" + "Monitor" + "<br>" + "Out :"+ "<br>" + "Printer" + "<br>" + "Out :"+ "<br>" + "Mouse"+ "<br>"  + "Out :"+ "<br>" + "Keyboard" ]
+document.write(another)
