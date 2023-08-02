029201f49a34989a5941ac5e737ec139bacc24f5
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:47:19 +0500

    Javascript Assignment 10
    Javascript Assignment 10

---
+++ javascript Assignment 10/app.js
@@ -0,0 +1,32 @@
+// 1. Show an alert box on click on a link.
+
+// function avatar()
+//     {
+// alert()
+//     }
+
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+// function hover(){
+// alert("This is a avatar")
+// }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+  
+// function over(){
+//     document.getElementById("change").src = "/girl.jpg"
+// }
+// function overing(){
+//     document.getElementById("change").src = "/avatar3.png"
+// }
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
