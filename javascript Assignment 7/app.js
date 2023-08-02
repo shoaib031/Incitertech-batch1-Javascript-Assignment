a526f3f7712e3447ca86660ca9422c22ce668e56
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:45:52 +0500

    Javascript Assignment 7
    Javascript Assignment 7

---
+++ javascript Assignment 7/app.js
@@ -0,0 +1,35 @@
+var num = parseFloat(prompt("Enter a negative floating-point number:")); 
+
+document.write("<h4>Number:</h4><p>" + num + "</p>"); 
+
+var rounded = Math.round(num); 
+document.write("<h4>Rounded value:</h4><p>" + rounded + "</p>"); 
+
+var floored = Math.floor(num); 
+document.write("<h4>Floor value:</h4><p>" + floored + "</p>"); 
+
+var ceiled = Math.ceil(num); 
+document.write("<h4>Ceil value:</h4><p>" + ceiled + "</p>"); 
+
+var num = parseFloat(prompt("Enter a Positive floating-point number:")); 
+
+document.write("<h4>Number:</h4><p>" + num + "</p>"); 
+
+var rounded = Math.round(num); 
+document.write("<h4>Rounded value:</h4><p>" + rounded + "</p>"); 
+
+var floored = Math.floor(num); 
+document.write("<h4>Floor value:</h4><p>" + floored + "</p>"); 
+
+var ceiled = Math.ceil(num); 
+document.write("<h4>Ceil value:</h4><p>" + ceiled + "</p>"); 
+
+var num = -4; 
+var abs = Math.abs(num); 
+document.write("The absolute value of " + num + " is " + abs); 
+
+var dice = Math.floor(Math.random() * 6) + 1; 
+
+document.write("<h4>Value of dice:</h4><p>" + dice + "</p>"); 
+
+// sir baki question main problem horhy hai
