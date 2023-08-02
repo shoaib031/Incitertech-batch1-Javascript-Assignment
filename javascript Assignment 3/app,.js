aa77732feede0281b3dac5bf0d92a8f0dd83d168
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-02 14:43:07 +0500

    Javascript Assignment 3
    Javascript Assignment 3

---
+++ javascript Assignment 3/app,.js
@@ -0,0 +1,700 @@
+var num = prompt("Enter your number");
+var num2 = prompt("Enter your number");
+
+if(num == 3){
+    document.write("Check Whether" + "<br />");
+}
+else{
+    document.write("Number is not divisble  by 3." + "<br />");
+}
+
+var A = prompt("Enter Number");
+var B = prompt("Enter Number");
+
+if(A == 2 || B == 3){
+    document.write("Is Odd Number" + "<br />");
+}
+if (A == 2 || B == 3){
+    document.write("Is Even Number" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var Age = prompt("Enter Your Age");
+
+if(Age == 18){
+    document.write("Too young" + "<br />");
+}
+else{
+    document.write("Old enough" + "<br />");
+}
+
+var userName = prompt("Enter Your Name");
+
+if(userName == "Mughess"){
+    document.write("It's your Mughess" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var number=prompt("Enter your number");
+var number2=prompt("Enter your number");
+number=Number(number);
+number2=Number(number2);
+var div=(number/number2);
+document.write(number +" / "+number2+" = "+div +"<br />");
+
+if( number == number2){ 
+    document.write("Number is not divisible by 3." + "<br />");
+}
+else{
+    document.write("Number is divisible by 3." + "<br />");
+}
+
+var A =prompt ("Please enter your number") ;
+var Z =prompt ("Please enter your number") ;
+var a =prompt ("Please enter your number") ;
+var z =prompt ("Please enter your number") ;
+
+if(A == 65 >= Z == 90){
+    document.write("Is a upper case letter" + "<br />");
+}
+else if(a == 97 >= z == 122){
+    document.write("Is a lower case letter" + "<br />");
+}
+
+var number = prompt("Enter num")
+var number2 = prompt("Enter num")
+var num = 1 ;
+var num2 = 2 ;
+var num3 = 3 ;
+var num4 = 4 ;
+var num5 = 5 ;
+var number = Number(number);
+var number2 = Number(number2);
+var num = Number(num)
+var num2 = Number(num2)
+var num3 = Number(num3);
+var num4 = Number(num4);
+var num5 = Number(num5);
+var add = (num + num2);
+var sub = (num - num2);
+var mul = (num * num2);
+var div = (num / num2);
+var per = (num % num2);
+
+switch(num){
+    case 0 :
+        document.write(number +" + "+number2 +" = "+add +"<br />");
+    break;
+    case 1 :    
+        document.write(number +" - "+number2 +" = "+sub +"<br />");
+    break;
+    case 2 :   
+        document.write(number +" * "+number2 +" = "+mul +"<br />");
+    break;
+    case 3 :    
+        document.write(number +" / "+number2 +" = "+div +"<br />");
+    break;
+    case 4 :    
+        document.write(number +" % "+number2 +" = "+per +"<br />");
+    break;
+}
+
+var password = prompt("Enter Your Password");
+
+if(password == "1234"){
+    document.write("Correct Password" + "<br />");
+}
+else if(password == "Moheed"){
+    document.write("Password" + "<br />");
+}
+else{
+    document.write("Please enter your password" + "<br />");
+}    
+
+var firstName = "Ali";
+
+if (firstName === "Fahad") {
+    document.write("Hello Fahad!" + "<br />");
+}
+else{
+    document.write("You are not Fahad" + "<br />");
+}
+
+var hour = 13;
+
+if(hour <= 18){
+    document.write("Good day" + "<br />");
+}
+else{
+    document.write("Good evening" + "<br />");
+} 
+
+var firstName = "Moheed";
+var secondName = "Siddiqui";
+
+if(firstName === secondName){
+    document.write("Are also equal" + "<br />");
+}
+
+var user = prompt("Please eneter your number");
+
+if(user == 0){
+    document.write("It's is positive" + "<br />");
+}
+else if(user == 1){
+    document.write("It's is negative" + "<br />");
+}
+else{
+    document.write("It is zero" + "<br />");
+}
+
+var currentHour = prompt("Enter time");
+var Hour = prompt("Enter time");
+
+if(currentHour == 6 + "a.m" && Hour == 9 + "a.m"){
+    document.write("Breakfast is served." + "<br />");
+}
+else if(currentHour == 11 + "a.m" && Hour == 1 + "p.m"){
+    document.write("Time for lunch" + "<br />");
+}
+else if(currentHour == 5 + "p.m" && Hour == 8 + "p.m"){
+    document.write("It's dinner time" + "<br />");
+}
+else{
+    document.write("Sorry, you'll have to wait, or go get a snack" + "<br />");
+}
+
+var number = 35 ; 
+var strings = "Strings"
+var boolean = "True";
+var undefined ;
+
+document.write(number + "<br />");
+document.write(strings + "<br />");
+document.write(boolean + "<br />");
+document.write(undefined + "<br />");
+
+var num = 10 ; 
+var num2 = 8 ;
+
+if(num != 8){
+    document.write("True" + "<br />");
+}
+
+var month = 10 ;
+
+if(month == 1){
+    alert("January");
+}
+else if(month == 2){
+    alert("February");
+}
+else if(month == 3){
+    alert("March");
+}
+else if(month == 4){
+    alert("April");
+}
+else if(month == 5){
+    alert("May");
+}
+else if(month == 6){
+    alert("June");
+}
+else if(month == 7){
+    alert("July");
+}
+else if(month == 8){
+    alert("Augest");
+}
+else if(month == 9){
+    alert("September");
+}
+else if(month == 10){
+    alert("October");
+}
+else if(month == 11){
+    alert("November");
+}
+else if(month == 12){
+    alert("December");
+}
+else{
+    alert("Invalid month");
+}
+
+var A = 18;
+var B = 
+
+(A <= 15)? B = "Too Young" : B = "Old enough";
+document.write(B);var num = prompt("Enter your number");
+var num2 = prompt("Enter your number");
+
+if(num == 3){
+    document.write("Check Whether" + "<br />");
+}
+else{
+    document.write("Number is not divisble  by 3." + "<br />");
+}
+
+var A = prompt("Enter Number");
+var B = prompt("Enter Number");
+
+if(A == 2 || B == 3){
+    document.write("Is Odd Number" + "<br />");
+}
+if (A == 2 || B == 3){
+    document.write("Is Even Number" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var Age = prompt("Enter Your Age");
+
+if(Age == 18){
+    document.write("Too young" + "<br />");
+}
+else{
+    document.write("Old enough" + "<br />");
+}
+
+var userName = prompt("Enter Your Name");
+
+if(userName == "Mughess"){
+    document.write("It's your Mughess" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var number=prompt("Enter your number");
+var number2=prompt("Enter your number");
+number=Number(number);
+number2=Number(number2);
+var div=(number/number2);
+document.write(number +" / "+number2+" = "+div +"<br />");
+
+if( number == number2){ 
+    document.write("Number is not divisible by 3." + "<br />");
+}
+else{
+    document.write("Number is divisible by 3." + "<br />");
+}
+
+var A =prompt ("Please enter your number") ;
+var Z =prompt ("Please enter your number") ;
+var a =prompt ("Please enter your number") ;
+var z =prompt ("Please enter your number") ;
+
+if(A == 65 >= Z == 90){
+    document.write("Is a upper case letter" + "<br />");
+}
+else if(a == 97 >= z == 122){
+    document.write("Is a lower case letter" + "<br />");
+}
+
+var number = prompt("Enter num")
+var number2 = prompt("Enter num")
+var num = 1 ;
+var num2 = 2 ;
+var num3 = 3 ;
+var num4 = 4 ;
+var num5 = 5 ;
+var number = Number(number);
+var number2 = Number(number2);
+var num = Number(num)
+var num2 = Number(num2)
+var num3 = Number(num3);
+var num4 = Number(num4);
+var num5 = Number(num5);
+var add = (num + num2);
+var sub = (num - num2);
+var mul = (num * num2);
+var div = (num / num2);
+var per = (num % num2);
+
+switch(num){
+    case 0 :
+        document.write(number +" + "+number2 +" = "+add +"<br />");
+    break;
+    case 1 :    
+        document.write(number +" - "+number2 +" = "+sub +"<br />");
+    break;
+    case 2 :   
+        document.write(number +" * "+number2 +" = "+mul +"<br />");
+    break;
+    case 3 :    
+        document.write(number +" / "+number2 +" = "+div +"<br />");
+    break;
+    case 4 :    
+        document.write(number +" % "+number2 +" = "+per +"<br />");
+    break;
+}
+
+var password = prompt("Enter Your Password");
+
+if(password == "1234"){
+    document.write("Correct Password" + "<br />");
+}
+else if(password == "Moheed"){
+    document.write("Password" + "<br />");
+}
+else{
+    document.write("Please enter your password" + "<br />");
+}    
+
+var firstName = "Ali";
+
+if (firstName === "Fahad") {
+    document.write("Hello Fahad!" + "<br />");
+}
+else{
+    document.write("You are not Fahad" + "<br />");
+}
+
+var hour = 13;
+
+if(hour <= 18){
+    document.write("Good day" + "<br />");
+}
+else{
+    document.write("Good evening" + "<br />");
+} 
+
+var firstName = "Moheed";
+var secondName = "Siddiqui";
+
+if(firstName === secondName){
+    document.write("Are also equal" + "<br />");
+}
+
+var user = prompt("Please eneter your number");
+
+if(user == 0){
+    document.write("It's is positive" + "<br />");
+}
+else if(user == 1){
+    document.write("It's is negative" + "<br />");
+}
+else{
+    document.write("It is zero" + "<br />");
+}
+
+var currentHour = prompt("Enter time");
+var Hour = prompt("Enter time");
+
+if(currentHour == 6 + "a.m" && Hour == 9 + "a.m"){
+    document.write("Breakfast is served." + "<br />");
+}
+else if(currentHour == 11 + "a.m" && Hour == 1 + "p.m"){
+    document.write("Time for lunch" + "<br />");
+}
+else if(currentHour == 5 + "p.m" && Hour == 8 + "p.m"){
+    document.write("It's dinner time" + "<br />");
+}
+else{
+    document.write("Sorry, you'll have to wait, or go get a snack" + "<br />");
+}
+
+var number = 35 ; 
+var strings = "Strings"
+var boolean = "True";
+var undefined ;
+
+document.write(number + "<br />");
+document.write(strings + "<br />");
+document.write(boolean + "<br />");
+document.write(undefined + "<br />");
+
+var num = 10 ; 
+var num2 = 8 ;
+
+if(num != 8){
+    document.write("True" + "<br />");
+}
+
+var month = 10 ;
+
+if(month == 1){
+    alert("January");
+}
+else if(month == 2){
+    alert("February");
+}
+else if(month == 3){
+    alert("March");
+}
+else if(month == 4){
+    alert("April");
+}
+else if(month == 5){
+    alert("May");
+}
+else if(month == 6){
+    alert("June");
+}
+else if(month == 7){
+    alert("July");
+}
+else if(month == 8){
+    alert("Augest");
+}
+else if(month == 9){
+    alert("September");
+}
+else if(month == 10){
+    alert("October");
+}
+else if(month == 11){
+    alert("November");
+}
+else if(month == 12){
+    alert("December");
+}
+else{
+    alert("Invalid month");
+}
+
+var A = 18;
+var B = 
+
+(A <= 15)? B = "Too Young" : B = "Old enough";
+document.write(B);var num = prompt("Enter your number");
+var num2 = prompt("Enter your number");
+
+if(num == 3){
+    document.write("Check Whether" + "<br />");
+}
+else{
+    document.write("Number is not divisble  by 3." + "<br />");
+}
+
+var A = prompt("Enter Number");
+var B = prompt("Enter Number");
+
+if(A == 2 || B == 3){
+    document.write("Is Odd Number" + "<br />");
+}
+if (A == 2 || B == 3){
+    document.write("Is Even Number" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var Age = prompt("Enter Your Age");
+
+if(Age == 18){
+    document.write("Too young" + "<br />");
+}
+else{
+    document.write("Old enough" + "<br />");
+}
+
+var userName = prompt("Enter Your Name");
+
+if(userName == "Mughess"){
+    document.write("It's your Mughess" + "<br />");
+}
+else{
+    document.write("Invalid" + "<br />");
+}
+
+var number=prompt("Enter your number");
+var number2=prompt("Enter your number");
+number=Number(number);
+number2=Number(number2);
+var div=(number/number2);
+document.write(number +" / "+number2+" = "+div +"<br />");
+
+if( number == number2){ 
+    document.write("Number is not divisible by 3." + "<br />");
+}
+else{
+    document.write("Number is divisible by 3." + "<br />");
+}
+
+var A =prompt ("Please enter your number") ;
+var Z =prompt ("Please enter your number") ;
+var a =prompt ("Please enter your number") ;
+var z =prompt ("Please enter your number") ;
+
+if(A == 65 >= Z == 90){
+    document.write("Is a upper case letter" + "<br />");
+}
+else if(a == 97 >= z == 122){
+    document.write("Is a lower case letter" + "<br />");
+}
+
+var number = prompt("Enter num")
+var number2 = prompt("Enter num")
+var num = 1 ;
+var num2 = 2 ;
+var num3 = 3 ;
+var num4 = 4 ;
+var num5 = 5 ;
+var number = Number(number);
+var number2 = Number(number2);
+var num = Number(num)
+var num2 = Number(num2)
+var num3 = Number(num3);
+var num4 = Number(num4);
+var num5 = Number(num5);
+var add = (num + num2);
+var sub = (num - num2);
+var mul = (num * num2);
+var div = (num / num2);
+var per = (num % num2);
+
+switch(num){
+    case 0 :
+        document.write(number +" + "+number2 +" = "+add +"<br />");
+    break;
+    case 1 :    
+        document.write(number +" - "+number2 +" = "+sub +"<br />");
+    break;
+    case 2 :   
+        document.write(number +" * "+number2 +" = "+mul +"<br />");
+    break;
+    case 3 :    
+        document.write(number +" / "+number2 +" = "+div +"<br />");
+    break;
+    case 4 :    
+        document.write(number +" % "+number2 +" = "+per +"<br />");
+    break;
+}
+
+var password = prompt("Enter Your Password");
+
+if(password == "1234"){
+    document.write("Correct Password" + "<br />");
+}
+else if(password == "Moheed"){
+    document.write("Password" + "<br />");
+}
+else{
+    document.write("Please enter your password" + "<br />");
+}    
+
+var firstName = "Ali";
+
+if (firstName === "Fahad") {
+    document.write("Hello Fahad!" + "<br />");
+}
+else{
+    document.write("You are not Fahad" + "<br />");
+}
+
+var hour = 13;
+
+if(hour <= 18){
+    document.write("Good day" + "<br />");
+}
+else{
+    document.write("Good evening" + "<br />");
+} 
+
+var firstName = "Moheed";
+var secondName = "Siddiqui";
+
+if(firstName === secondName){
+    document.write("Are also equal" + "<br />");
+}
+
+var user = prompt("Please eneter your number");
+
+if(user == 0){
+    document.write("It's is positive" + "<br />");
+}
+else if(user == 1){
+    document.write("It's is negative" + "<br />");
+}
+else{
+    document.write("It is zero" + "<br />");
+}
+
+var currentHour = prompt("Enter time");
+var Hour = prompt("Enter time");
+
+if(currentHour == 6 + "a.m" && Hour == 9 + "a.m"){
+    document.write("Breakfast is served." + "<br />");
+}
+else if(currentHour == 11 + "a.m" && Hour == 1 + "p.m"){
+    document.write("Time for lunch" + "<br />");
+}
+else if(currentHour == 5 + "p.m" && Hour == 8 + "p.m"){
+    document.write("It's dinner time" + "<br />");
+}
+else{
+    document.write("Sorry, you'll have to wait, or go get a snack" + "<br />");
+}
+
+var number = 35 ; 
+var strings = "Strings"
+var boolean = "True";
+var undefined ;
+
+document.write(number + "<br />");
+document.write(strings + "<br />");
+document.write(boolean + "<br />");
+document.write(undefined + "<br />");
+
+var num = 10 ; 
+var num2 = 8 ;
+
+if(num != 8){
+    document.write("True" + "<br />");
+}
+
+var month = 10 ;
+
+if(month == 1){
+    alert("January");
+}
+else if(month == 2){
+    alert("February");
+}
+else if(month == 3){
+    alert("March");
+}
+else if(month == 4){
+    alert("April");
+}
+else if(month == 5){
+    alert("May");
+}
+else if(month == 6){
+    alert("June");
+}
+else if(month == 7){
+    alert("July");
+}
+else if(month == 8){
+    alert("Augest");
+}
+else if(month == 9){
+    alert("September");
+}
+else if(month == 10){
+    alert("October");
+}
+else if(month == 11){
+    alert("November");
+}
+else if(month == 12){
+    alert("December");
+}
+else{
+    alert("Invalid month");
+}
+
+var A = 18;
+var B = 
+
+(A <= 15)? B = "Too Young" : B = "Old enough";
+document.write(B);
